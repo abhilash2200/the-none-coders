9e6362abac12941b071e1a254a6a1d5b9464abe0
abhilash2200 <ab.digitalwolf@gmail.com>
2025-08-26 13:46:49 +0530

    make updte responsive

--- app/updates/page.tsx
+++ app/updates/page.tsx
@@ -15,7 +15,20 @@ import Stack from "@mui/material/Stack";
 function Page() {
     const [loading, setLoading] = useState(true);
     const [currentPage, setCurrentPage] = useState(1);
-    const blogsPerPage = 50;
+    const [blogsPerPage, setBlogsPerPage] = useState(50);
+    useEffect(() => {
+        const updateBlogsPerPage = () => {
+            if (window.innerWidth < 768) {
+                setBlogsPerPage(20);
+            } else {
+                setBlogsPerPage(50);
+            }
+        };
+        updateBlogsPerPage();
+        window.addEventListener("resize", updateBlogsPerPage);
+
+        return () => window.removeEventListener("resize", updateBlogsPerPage);
+    }, []);
 
     const totalPages = Math.ceil(updatesList.length / blogsPerPage);
     const indexOfLastBlog = currentPage * blogsPerPage;
@@ -23,9 +36,7 @@ function Page() {
     const currentBlogs = updatesList.slice(indexOfFirstBlog, indexOfLastBlog);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 2000);
+        const timer = setTimeout(() => setLoading(false), 2000);
         return () => clearTimeout(timer);
     }, []);
 
@@ -38,77 +49,74 @@ function Page() {
     };
 
     return (
-        <>
-            <section className="py-6 lg:py-12">
-                <div className="container mx-auto px-4">
-                    <div className="mb-6 lg:mb-10">
-                        <HeadingText textalign="text-center" heading="UPDATES" />
-                    </div>
-
-                    {loading ? (
-                        <SkeletonUpdates />
-                    ) : (
-                        <>
-                            <div className="flex flex-wrap gap-y-6 gap-x-2">
-                                {currentBlogs.map((update: UpdateListItem) => (
-                                    <div
-                                        key={update.slug}
-                                        className="w-full md:w-[49%] lg:w-[32.33%] md:p-4 p-2 rounded-md border-1 border-[#cacaca]"
-                                    >
-                                        <Image
-                                            src={update.image}
-                                            alt={update.title}
-                                            width={450}
-                                            height={250}
-                                            className="rounded-md mb-2 transition-transform duration-300 ease-in-out group-hover:scale-101 group-hover:shadow-md"
-                                        />
-
-                                        <div className="flex justify-between items-center mb-4">
-                                            <p className="text-gray-500 text-sm inline-flex justify-center items-center gap-x-1">
-                                                <TerminalIcon />
-                                                <span className="text-gray-800 font-medium">
-                                                    {update.info}
-                                                </span>
-                                            </p>
-                                            <p className="text-gray-500 text-sm inline-flex justify-center items-center gap-x-1">
-                                                <CalendarMonthOutlinedIcon />
-                                                <span className="text-gray-800 font-medium">
-                                                    {update.date}
-                                                </span>
-                                            </p>
-                                        </div>
-                                        <Link href={`/updates/${update.slug}`}>
-                                            <h2 className="text-xl font-semibold mt-3 hover:text-[#2D176A]">
-                                                {update.title}
-                                            </h2>
-                                        </Link>
-                                        <p className="text-gray-600 text-sm">{update.description}</p>
-                                        <div className="mt-5 flex flex-start">
-                                            <Link href={`/updates/${update.slug}`}>
-                                                <ArrowButton />
-                                            </Link>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-
-                            <div className="flex justify-center mt-8">
-                                <Stack spacing={2}>
-                                    <Pagination
-                                        count={totalPages}
-                                        page={currentPage}
-                                        onChange={handlePageChange}
-                                        variant="outlined"
-                                        shape="rounded"
-                                        color="standard"
-                                    />
-                                </Stack>
-                            </div>
-                        </>
-                    )}
+        <section className="py-6 lg:py-12">
+            <div className="container mx-auto px-4">
+                <div className="mb-6 lg:mb-10">
+                    <HeadingText textalign="text-center" heading="UPDATES" />
                 </div>
-            </section>
-        </>
+
+                {loading ? (
+                    <SkeletonUpdates />
+                ) : (
+                    <>
+                        <div className="flex flex-wrap gap-y-6 gap-x-2">
+                            {currentBlogs.map((update: UpdateListItem) => (
+                                <div
+                                    key={update.slug}
+                                    className="w-full md:w-[49%] lg:w-[32.33%] md:p-4 p-2 rounded-md border-1 border-[#cacaca]"
+                                >
+                                    <Image
+                                        src={update.image}
+                                        alt={update.title}
+                                        width={450}
+                                        height={250}
+                                        className="rounded-md mb-2 transition-transform duration-300 ease-in-out group-hover:scale-101 group-hover:shadow-md"
+                                    />
+                                    <div className="flex justify-between items-center mb-4">
+                                        <p className="text-gray-500 text-sm inline-flex justify-center items-center gap-x-1">
+                                            <TerminalIcon />
+                                            <span className="text-gray-800 font-medium">
+                                                {update.info}
+                                            </span>
+                                        </p>
+                                        <p className="text-gray-500 text-sm inline-flex justify-center items-center gap-x-1">
+                                            <CalendarMonthOutlinedIcon />
+                                            <span className="text-gray-800 font-medium">
+                                                {update.date}
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <Link href={`/updates/${update.slug}`}>
+                                        <h2 className="text-xl font-semibold mt-3 hover:text-[#2D176A]">
+                                            {update.title}
+                                        </h2>
+                                    </Link>
+                                    <p className="text-gray-600 text-sm">{update.description}</p>
+                                    <div className="mt-5 flex flex-start">
+                                        <Link href={`/updates/${update.slug}`}>
+                                            <ArrowButton />
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="flex justify-center mt-8">
+                            <Stack spacing={2}>
+                                <Pagination
+                                    count={totalPages}
+                                    page={currentPage}
+                                    onChange={handlePageChange}
+                                    variant="outlined"
+                                    shape="rounded"
+                                    color="standard"
+                                />
+                            </Stack>
+                        </div>
+                    </>
+                )}
+            </div>
+        </section>
     );
 }
 
